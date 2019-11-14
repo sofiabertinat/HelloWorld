2f44d10004fd89f8fda684aeece95603ae35905e
Filipe Sousa <filipe.monteiro.sousa@gmail.com>
2019-11-14 00:18:32 +0000

    Included safe measures to allocate memory

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "greetings.h"
 int main(void){
-	char greeting[20];
-	get_greeting(greeting);
-	printf("Hello world!\n");
-	printf("%s", greeting);
-	return 0;
+	char * greeting;
+	if (get_greetingSize()>0) {
+		greeting = (char *) malloc(get_greetingSize()/sizeof(char));
+	}
+	else {
+		printf("Greeting string is to short!\n");
+		return -1;
+	}
+
+	if (get_greeting(greeting)==0) {
+		printf("%s", greeting);
+		return 0;
+	}
+	else return -1;
 }
 
